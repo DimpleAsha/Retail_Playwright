3abe1f8163dcc034dc4397df7b8eb5d8f28804b4
Dimplegurajala <dimplegurajala@gmail.com>
2026-06-17 13:22:18 +0530

    CI: push the latest code changes

--- tests/e2eTest1.spec.ts
+++ tests/e2eTest1.spec.ts
@@ -1,10 +1,17 @@
 import { fixtures as test } from '../fixtures.js';
-import {expect} from '@playwright/test'
+import { expect } from '@playwright/test';
 
 test('Retail Flow: Search and Add to Cart', async ({ loginPage, searchPage, addToCartPage, page }) => {
-  await loginPage.navigateTo('/login');
-  await loginPage.login('user@test.com', 'securePass');
+  // Using environment variables for URL and Credentials
+  const url = process.env.UI_URL || 'https://default-url.com';
+  const user = process.env.UI_USERNAME!;
+  const pass = process.env.UI_PASSWORD!;
+
+  await loginPage.navigateTo(url);
+  await loginPage.login(user, pass);
+  
   await searchPage.search('Laptop');
   await addToCartPage.addToCart('Laptop');
+  
   await expect(page.locator('.cart-count')).toHaveText('1');
 });
